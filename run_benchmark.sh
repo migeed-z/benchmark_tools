5aea733b46f74f2924abcb62f9a9d785f72ee05b
zeina migeed <migeed.z@outlook.com>
2016-08-01 12:55:24 -0400

    modify script to run on cluster

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,6 +1,6 @@
 
-# ELAP_TIME=$( qstat -u bengree | awk '{if ($1 == "1661991.m2") print $11;}' )
-ELAP_TIME=$( cat 'SAMPLE_QSTAT.txt' | awk '{if ($1 == "1661991.m2") print $11;}' )
+# ELAP_TIME=$( qstat -u bengree | awk '{if ($1 == ${PBS_JOBID}) print $11;}' )
+ELAP_TIME=$( qstat -u bengree | awk '{if ($1 == ${PBS_JOBID}) print $11;}' )
 TIME_TO_STOP="23:00:00"
 
 if [ ${ELAP_TIME} \> ${TIME_TO_STOP} ]; then
