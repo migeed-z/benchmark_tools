cbee62032ad229a765e3097b267a6d72e9ec334a
Ben Greenman <benjaminlgreenman@gmail.com>
2016-08-01 12:05:32 -0400

    sample data for karst script

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,3 +1,5 @@
-qstat -u bengree | awk '{if ($1 == "1661991.m2") print $9;}'
 
+# ELAP_TIME=$( qstat -u bengree | awk '{if ($1 == "1661991.m2") print $11;}' )
+ELAP_TIME=$( cat 'SAMPLE_QSTAT.txt' | awk '{if ($1 == "1661991.m2") print $11;}' )
 
+echo ${ELAP_TIME}
