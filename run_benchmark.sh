cf3323dd1828d8c4953bbf91958ac4b05c05aaba
Ben Greenman <benjaminlgreenman@gmail.com>
2016-08-01 12:34:08 -0400

    [karst] compare strings, choose whether to stop running

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,5 +1,10 @@
 
 # ELAP_TIME=$( qstat -u bengree | awk '{if ($1 == "1661991.m2") print $11;}' )
 ELAP_TIME=$( cat 'SAMPLE_QSTAT.txt' | awk '{if ($1 == "1661991.m2") print $11;}' )
+TIME_TO_STOP="23:00:00"
 
-echo ${ELAP_TIME}
+if [ ${ELAP_TIME} \> ${TIME_TO_STOP} ]; then
+  echo "STOP RUNNING CONFIGS"
+else
+  echo "PICK AND RUN A CONFIG"
+fi
